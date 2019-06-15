ff88ec1f4667397b00fd3159f3bc336dbe0aace7
Jianhua Cheng <w.chengjianhua@gmail.com>
2019-06-15 10:25:40 +0800

    docs: explain the goal of different pages

---
+++ src/pages/question-groups/README.md
@@ -0,0 +1,7 @@
+# question-groups 题目类型
+
+例如：
+
+- 算法空间
+- Python 工厂
+- 萌新乐园
---
+++ src/pages/question-groups/question-groups.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import styled from 'styled-components';
+import AV from 'leancloud-storage';
+
+const Root = styled.div``;
+
+export default function QuestiongGroups() {
+  const [loading, setLoading] = React.useState(false);
+
+  return <Root />;
+}
